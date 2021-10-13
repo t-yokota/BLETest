81e6b458efd0d9742a53eda9a95c1466902e37e9
tyokota <yokota.0529@gmail.com>
2021-10-14 07:36:04 +0900

    added BLE related functions (scan to bond) based on https://punchthrough.com/android-ble-guide/

--- app/src/main/java/com/example/bletest/MainActivity.kt
+++ app/src/main/java/com/example/bletest/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.example.bletest
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.*
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,7 +22,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.bletest.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,9 +48,16 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener { //view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+            if (isScanning) {
+                stopBleScan()
+//                findViewById<TextView>(R.id.scanStateText).setText(R.string.Stopped)
+            } else {
+                startBleScan()
+//                findViewById<TextView>(R.id.scanStateText).setText(R.string.Scanning)
+            }
         }
     }
 
@@ -54,5 +81,224 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+//--------------------------------------------------------------------------------------------------
+    /* Based on "The Ultimate Guide to Android Bluetooth Low Energy" */
+    /* https://punchthrough.com/android-ble-guide/ */
+
+    /* Making sure that Bluetooth is enabled */
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+    }
+
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            ENABLE_BLUETOOTH_REQUEST_CODE -> {
+                if (resultCode != Activity.RESULT_OK) {
+//                    promptEnableBluetooth()
+                    Log.i("ble_test","BLE is not enabled.")
+                } else {
+                    Log.i("ble_test","BLE is enabled.")
+                }
+            }
+        }
+    }
+
+    /* Requesting location permission from the user */
+    val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted) {
+            return
+        }
+        Log.i("ble_test","Fine location permission is required.")
+        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_REQUEST_CODE)
+//        runOnUiThread {
+//            alert {
+//                title = "Location permission required"
+//                message = "Starting from Android M (6.0), the system requires apps to be granted " +
+//                        "location access in order to scan for BLE devices."
+//                isCancelable = false
+//                positiveButton(android.R.string.ok) {
+//                    requestPermission(
+//                        Manifest.permission.ACCESS_FINE_LOCATION,
+//                        LOCATION_PERMISSION_REQUEST_CODE
+//                    )
+//                }
+//            }.show()
+//        }
+    }
+
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+//                    requestLocationPermission()
+                }
+            }
+        }
+    }
+
+    /* Performing a BLE scan */
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+
+    private var isScanning = false
+        set(value) {
+            field = value
+            if (value) {
+                findViewById<TextView>(R.id.scanStateText).setText(R.string.Scanning)
+            } else {
+                findViewById<TextView>(R.id.scanStateText).setText(R.string.Stopped)
+            }
+        }
+
+    private fun startBleScan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
+            requestLocationPermission()
+        }
+        else {
+            Log.i("ble_test", "scanning...")
+            bleScanner.startScan(null, scanSettings, scanCallback)
+            isScanning = true
+        }
+    }
+
+    private fun stopBleScan() {
+        Log.i("ble_test", "stopped scan.")
+        bleScanner.stopScan(scanCallback)
+        isScanning = false
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            with(result.device) {
+                if (name != null) {
+                    Log.i("ble_test", "ScanCallback: Name: ${name ?: "Unnamed"}, address: $address")
+                    /*  */
+                    if (name == "Feather nRF52840 Express") {
+                        stopBleScan()
+                        Log.i("ble_test", "ScanCallback: Connecting to \"$name\", address: $address")
+                        connectGatt(applicationContext, false, gattCallback)
+                    }
+                }
+            }
+        }
+    }
+
+    /* Connecting to a BLE device */ /* Discovering services */
+    private val gattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            val deviceAddress = gatt.device.address
+
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.i("ble_test", "GattCallback: Successfully connected to $deviceAddress")
+                    // TODO: Store a reference to BluetoothGatt
+                    /*  */
+                    gatt.discoverServices()
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.i("ble_test", "GattCallback: Successfully disconnected from $deviceAddress")
+                    gatt.close()
+                }
+            } else {
+                Log.w("ble_test", "GattCallback: Error $status encountered for $deviceAddress! Disconnecting...")
+//                gatt.close()
+                gatt.connect()
+            }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            with(gatt) {
+                Log.i("ble_test", "GattCallback: Discovered ${services.size} services for ${device.address}")
+                printGattTable() // See implementation just above this section
+                // Consider connection setup as complete here
+                /*   */
+                listenToBondStateChanges(applicationContext)
+                bluetoothAdapter.getRemoteDevice(device.address).createBond()
+            }
+        }
+    }
+
+    private fun BluetoothGatt.printGattTable() {
+        if (services.isEmpty()) {
+            Log.i("ble_test", "printGattTable: No service and characteristic available, call discoverServices() first?")
+            return
+        }
+        services.forEach { service ->
+            val characteristicsTable = service.characteristics.joinToString(
+                separator = "\n|--",
+                prefix = "|--"
+            ) { it.uuid.toString() }
+            Log.i("ble_test", "printGattTable: \nService ${service.uuid}\nCharacteristics:\n$characteristicsTable"
+            )
+        }
+    }
+
+    /* Bonding with a BLE device */
+    fun listenToBondStateChanges(context: Context) {
+        context.applicationContext.registerReceiver(
+            broadcastReceiver,
+            IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
+        )
+    }
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            with(intent) {
+                if (action == BluetoothDevice.ACTION_BOND_STATE_CHANGED) {
+                    val device = getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                    val previousBondState = getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, -1)
+                    val bondState = getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1)
+                    val bondTransition = "${previousBondState.toBondStateDescription()} to " +
+                            bondState.toBondStateDescription()
+                    Log.i("ble_test", "Bond state change: ${device?.address} bond state changed | $bondTransition")
+                }
+            }
+        }
+
+        private fun Int.toBondStateDescription() = when(this) {
+            BluetoothDevice.BOND_BONDED -> "BONDED"
+            BluetoothDevice.BOND_BONDING -> "BONDING"
+            BluetoothDevice.BOND_NONE -> "NOT BONDED"
+            else -> "ERROR: $this"
+        }
     }
 }
